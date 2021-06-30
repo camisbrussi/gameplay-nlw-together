683a80a22c456fb812848ffb57cbac8aaeacf20e
camisbrussi <camila.sbrussi@universo.univates.br>
2021-06-30 20:16:23 -0300

    Projeto nlw together

---
+++ src/screens/Guilds/index.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+import { Text, View, FlatList } from 'react-native';
+
+import { styles } from './styles';
+import { theme } from '../../global/styles/theme';
+
+import { ListDivider } from '../../components/ListDivider';
+import { Guild, GuildProps } from '../../components/Guild';
+import { Load } from '../../components/Load';
+import { api } from '../../service/api';
+
+type Props = {
+  handleGuildSelect: (guild: GuildProps) => void;
+};
+
+export function Guilds({ handleGuildSelect }: Props) {
+  const [guilds, setGuilds] = useState<GuildProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchGuilds() {
+    const response = await api.get('/users/@me/guilds');
+
+    setGuilds(response.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchGuilds();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <Load />
+      ) : (
+        <FlatList
+          data={guilds}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Guild data={item} onPress={() => handleGuildSelect(item)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider isCentered />}
+          ListHeaderComponent={() => <ListDivider isCentered />}
+          contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+          style={styles.guilds}
+        />
+      )}
+    </View>
+  );
+}
